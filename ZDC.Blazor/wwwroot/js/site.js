fb8a67cf01e3f8e1d65f6eb13f681625be701a2a
Jacob Boyles <fixterjake@gmail.com>
2021-04-13 16:02:59 -0400

    Updates for sizing and background image

--- ZDC.Blazor/wwwroot/js/site.js
+++ ZDC.Blazor/wwwroot/js/site.js
@@ -1,3 +1,9 @@
-﻿window.setTitle = (title) => {
-    document.title = title;
-}
+﻿$(window).scroll(function () {
+    if (window.$(document).scrollTop() > 50) {
+        window.$("#home-nav").removeClass("transparent");
+        window.$("#home-nav").addClass("bg-gray-800");
+    } else {
+        window.$("#home-nav").addClass("transparent");
+        window.$("#home-nav").removeClass("bg-gray-800");
+    }
+});
